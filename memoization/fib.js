ec1b0685fc418c9ff5942d7497a902e5792adf7e
Pablo Motta <pmotta.dev@gmail.com>
2021-04-05 16:51:42 -0700

    organized memoization

---
+++ memoization/fib.js
@@ -0,0 +1,23 @@
+const fibInefficient = (n) => {
+	if ((n = 2)) return 1;
+	return fib(n - 1) + fib(n - 2);
+};
+
+console.log(fibInefficient(6));
+console.log(fibInefficient(7));
+console.log(fibInefficient(8));
+
+// Memoization
+//      js objects, keys will be arg to fn, value will be the return value
+
+const fib = (n, memo = {}) => {
+	if (n in memo) return memo[n];
+	if (n <= 2) return 1;
+
+	memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+	return memo[n];
+};
+console.log(fib(6));
+console.log(fib(7));
+console.log(fib(8));
+console.log(fib(50));
